3580f3a0de28ff2ca5d6e4d87c55d0833ea8d4a3
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-11 13:21:17 +0300

    add templates

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Kiprotich",
+        "title": "First Blog post",
+        "content": "My first content",
+        "date_posted": "April 20, 2019"
+    },
+    {
+        "author": "Kiprotich Dominic",
+        "title": "First Blog post",
+        "content": "My first content",
+        "date_posted": "April 20, 2020"
+    }
+]
+
 
 @app.route('/')
+@app.route("/home")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", posts = posts, title = "Home page")
+
 
 @app.route("/about")
-def about: render_template("about.html")
-    return
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
