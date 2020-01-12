6d77671c667d11cc1f418a71a43bb85480e7fdaa
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-12 10:56:58 +0300

    imporove the login system

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from app import app,db, bcrypt
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user,logout_user
 
 
 posts = [
@@ -34,6 +34,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(
@@ -49,12 +51,20 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter-by(email=form.email.data).first()
-        if userand bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember,data)
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('Login successfull.', 'success')
             return redirect(url_for("home"))
         else:           
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
