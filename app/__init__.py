b358daa120eccd775e7be4436c4b0365dfc44bf8
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-12 10:07:19 +0300

    initialised login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config["SECRET_KEY"] = "da3f071810a37f4d8984a3fca56014fe18f40a02"
 app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///site.db"
 db= SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from app import routes
