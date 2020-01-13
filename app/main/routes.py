f1c2268dde083b078cdcc3c535d303e05d16ecb1
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-13 23:03:17 +0300

    fix file structure

---
+++ app/main/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template, request, Blueprint
+from app.models import Post
+
+main = Blueprint("main",__name__)
+
+
+@main.route('/')
+@main.route("/home")
+def home():
+    posts = Post.query.all()
+    return render_template("index.html", posts=posts, title="Home page")
+
+
+@main.route("/about")
+def about():
+    return render_template("about.html")
+
--- app/people/forms.py
+++ app/people/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField,BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from app.models import User
+
 
 
 class RegistrationForm(FlaskForm):
@@ -52,12 +53,6 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError("That email is taken please choose another")
             
-
-class PostForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    content = TextAreaField("Content", validators=[DataRequired()])
-    submit = SubmitField("Post")
-
 class RequestResetForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     submit = SubmitField("Request Password Reset")
