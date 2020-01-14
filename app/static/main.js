613a4804826c54dae9126532da6b465cb7c3ab5f
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-01-14 12:05:44 +0300

    add like and dislike

---
+++ app/static/main.js
@@ -0,0 +1,10 @@
+let clicks = 0;
+function onClick() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+}
+
+function onClicks() {
+    clicks += 1;
+    document.getElementById("click").innerHTML = clicks;
+}
